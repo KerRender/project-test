d5e8f4171de0f17ac37f3c1f11213c18f1c8db6f
Max Pochepets <agreed_note@hotmail.com>
2020-11-22 16:12:39 +0200

    it`s working now

---
+++ src/main/java/stud/learn/Main.java
@@ -0,0 +1,19 @@
+package stud.learn;
+
+public class Main {
+
+    public static void main(String[] args) {
+        HelloWorld obj = new HelloWorld();
+        obj.Message();
+    }
+
+    public static class HelloWorld {
+
+
+        public void Message() {
+            System.out.println("Hello world!");
+            System.out.println("Hello mates!");
+        }
+
+    }
+}
